07de30400cbfb572c3ef3d48b92d85b2b1969f5f
Hamed <hamed.martino@gmail.com>
2020-10-18 17:23:17 -0700

    adding comments

--- dreamcatcher/oura.py
+++ dreamcatcher/oura.py
@@ -2,21 +2,37 @@
 
 import os
 import requests
-from flask import Flask, session, redirect, request, url_for
+from flask import Flask, session, redirect, request, url_for, jsonify
 from requests_oauthlib import OAuth2Session
 import pandas as pd
 from creds import OURA_CLIENT_ID, OURA_CLIENT_SECRET
+import json
 
 app = Flask(__name__)
 
-
+# Date range for sleep data
 START_DATE = '2020-01-01'
 END_DATE = '2020-10-18'
-LOCAL_STORAGE_PATH = 'sleep_data_from{}_to{}.csv'.format(START_DATE, END_DATE)
 
+# Where to store your data
+LOCAL_STORAGE_PATH = 'sleep_data_from{}_to{}.csv'.format(START_DATE, END_DATE)
+FULL_DATA = 'full_data_from{}_to{}.csv'.format(START_DATE, END_DATE)
+
+'''
+From the Oura website:
+
+If you are using an existing OAuth2 library, you may need to configure the following URLs.
+
+    Authorize: https://cloud.ouraring.com/oauth/authorize
+    Access Token URL: https://api.ouraring.com/oauth/token
+
+'''
 OURA_AUTH_URL = 'https://cloud.ouraring.com/oauth/authorize'
 OURA_TOKEN_URL = 'https://api.ouraring.com/oauth/token'
 
+
+# The requests-oauthlib library 
+# https://requests-oauthlib.readthedocs.io/en/latest/ 
 
 @app.route('/login')
 def oura_login():
@@ -30,6 +46,9 @@ def oura_login():
     # URL for Oura's authorization page for specific client
     authorization_url, state = oura_session.authorization_url(OURA_AUTH_URL)
 
+
+    # The session is the interval at which the client logs on to the server and logs out the server.
+    # The data that is required to be saved in the session is stored in a temporary directory on the server.
     session['oauth_state'] = state
 
     return redirect(authorization_url)
@@ -46,7 +65,7 @@ def callback():
                         OURA_TOKEN_URL,
                         client_secret=OURA_CLIENT_SECRET,
                         authorization_response=request.url)
-    return redirect(url_for('.sleep'))
+    return redirect(url_for('sleep'))
 
 
 @app.route('/sleep')
@@ -67,6 +86,23 @@ def sleep():
     df.to_csv(LOCAL_STORAGE_PATH)
     return '<p>Successfully stored sleep data</p><p>{}</p>'\
         .format(df.describe())
+
+
+# fullData shows that there is only one key in this JSON. 
+
+# @app.route('/fullData')
+# def fullData():
+#     oauth_token = session['oauth']['access_token']
+
+#     full_data = requests.get('https://api.ouraring.com/v1/sleep?start={}&end={}&access_token={}'.\
+#         format(START_DATE, END_DATE, oauth_token))
+
+#     json_fullData = full_data.json()
+
+#     for key, _ in json_fullData.items():
+#         print('The available data is: ' + key)
+
+#     return json_fullData
 
 
 
